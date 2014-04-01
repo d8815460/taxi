de1daa9832a21c9cda051b198eeab5a4ad2f068e
d8815460 <d8815460@yahoo.com.tw>
2014-04-01 14:31:46 +0800

    Initial Commit

---
+++ taxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  taxi
+//
+//  Created by Ayi on 2014/4/1.
+//  Copyright (c) 2014年 Miiitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ taxi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  taxi
+//
+//  Created by Ayi on 2014/4/1.
+//  Copyright (c) 2014年 Miiitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
